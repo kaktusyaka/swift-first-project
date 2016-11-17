c1b088208f91c32cb514d9d24620b1516e984081
Vitalya Shevtsov <vitaliy@cloudassess.com.au>
2016-11-18 07:42:24 +1000

    added ProductViewController

---
+++ GoodAsOldPhones/ProductViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProductViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Vitaliy Shevtsov on 17/11/16.
+//  Copyright © 2016 Vitaliy Shevtsov. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
