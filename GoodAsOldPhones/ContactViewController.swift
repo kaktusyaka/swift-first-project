d5c5484c0da062b6d716940e0c42bb32fbcb5692
Vitalya Shevtsov <vitaliy@cloudassess.com.au>
2016-11-24 12:44:50 +1000

    working with second view

---
+++ GoodAsOldPhones/ContactViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ContactViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Vitaliy Shevtsov on 24/11/16.
+//  Copyright © 2016 Vitaliy Shevtsov. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
