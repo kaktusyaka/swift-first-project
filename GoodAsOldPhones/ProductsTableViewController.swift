c512c666083b3aecde79841a3eeb47beead3119c
Vitalya Shevtsov <vitaliy@cloudassess.com.au>
2016-12-05 15:43:30 +1000

    create tableViewController

---
+++ GoodAsOldPhones/ProductsTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProductsTableViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Vitaliy Shevtsov on 5/12/16.
+//  Copyright © 2016 Vitaliy Shevtsov. All rights reserved.
+//
+
+import UIKit
+
+class ProductsTableViewController: UITableViewController {
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 5
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+        
+        return cell
+    }
+    
+}
